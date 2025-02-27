9bb21687beff80a45a9e7df3797f6648fb67df05
이주현 <cmh1199@naver.com>
2025-02-27 17:56:54 +0900

    feat: split 구현

--- NonLinear/BTree.java
+++ NonLinear/BTree.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class BTree {
     private int degrees;
+    private int minKeys;
     private int maxKeys;
     private Node root;
 
@@ -11,6 +12,7 @@ public class BTree {
         this.root = null;
         this.degrees = degrees;
         this.maxKeys = degrees - 1;
+        this.minKeys = degrees / 2;
     }
 
     public String search(int key) {
@@ -32,7 +34,7 @@ public class BTree {
 
     public void insert(int key, String address) {
         if(root == null) {
-            root = new Node(null);
+            root = new Node(null, true);
             root.keys.add(key);
             root.addresses.add(address);
             return;
@@ -43,7 +45,8 @@ public class BTree {
             int idx = -(Collections.binarySearch(current.keys, key) + 1);
             if(idx < 0) return;
             if (idx >= current.children.size()) {
-                current.children.add(new Node(current));
+                current.children.add(new Node(current, true));
+                current.isLeaf = false;
             }
             current = current.children.get(idx);
         }
@@ -58,7 +61,38 @@ public class BTree {
     }
 
     public void split(Node current) {
+        int midIdx = current.keys.size() / 2;
+        int midKey = current.keys.get(midIdx);
+        String midAddress = current.addresses.get(midIdx);
 
+        if(current == root) {
+            current.parent = new Node(null, false);
+        }
+        Node parent = current.parent;
+
+        int idx = -(Collections.binarySearch(parent.keys, midKey) + 1);
+        parent.keys.add(idx, midKey);
+        parent.addresses.add(idx, midAddress);
+
+        Node left = new Node(parent, current.isLeaf);
+        Node right = new Node(parent, current.isLeaf);
+
+        for(int i = 0; i < midIdx; i++) {
+            left.keys.add(current.keys.get(i));
+            left.addresses.add(current.addresses.get(i));
+            left.children.add(current.children.get(i));
+        }
+
+        for(int i = midIdx + 1; i < current.keys.size(); i++) {
+            right.keys.add(current.keys.get(i));
+            right.addresses.add(current.addresses.get(i));
+            right.children.add(current.children.get(i));
+        }
+
+        parent.children.add(idx, left);
+        parent.children.add(idx+1, right);
+
+        if(parent.keys.size() > maxKeys) split(parent);
     }
 
     public void delete(int key) {
@@ -72,11 +106,11 @@ public class BTree {
         boolean isLeaf;
         Node parent;
 
-        public Node(Node parent) {
+        public Node(Node parent, boolean isLeaf) {
             keys = new ArrayList<>();
             addresses = new ArrayList<>();
             children = new ArrayList<>();
-            isLeaf = true;
+            this.isLeaf = isLeaf;
             this.parent = parent;
         }
     }
