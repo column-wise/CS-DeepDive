bd70becb4741f08745d3c3b4f9469bc5c5d364f7
column-wise <cmh1199@naver.com>
2025-06-19 16:58:14 +0900

    refactor: Change Node class to abstract

--- Network/Node/Node.java
+++ Network/Node/Node.java
@@ -3,18 +3,12 @@ package Network.Node;
 import Network.DataUnit.DataUnit;
 import Network.Network.Network;
 
-public class Node {
+public abstract class Node {
     protected String ipAddress;
     protected String MACAddress;
     protected Network network;
 
-    public Node(String MACAddress, Network network) {
-        ipAddress = null;
-        this.MACAddress = MACAddress;
-        this.network = network;
-    }
-
     public void receive(DataUnit data) {
-        System.out.println(data.toString());
+        System.out.println(getClass().getName() + " " + ipAddress + " received " + data.toString());
     }
 }
