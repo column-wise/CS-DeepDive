4ebd3bebf053c95bbfddbbd57a0527f8d4f10d3c
column-wise <cmh1199@naver.com>
2025-07-03 13:42:36 +0900

    feat: Implement basic NAT table with dynamic port mapping

--- Network/Node/Core/Router.java
+++ Network/Node/Core/Router.java
@@ -5,19 +5,27 @@ import Network.Network.Subnet;
 import Network.Node.Node;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Supplier;
 
 public class Router extends Node {
+	private String externalIP = null;
 	List<Interface> interfaces;
 	NATTable natTable;
+	private final AtomicInteger portAllocator = new AtomicInteger(0);
 
 	public void addInterface(Interface iface) {
 		interfaces.add(iface);
+		if(iface.type == Interface.Type.PRIVATE && externalIP == null) externalIP = iface.ipAddress;
 	}
 
 	public Router() {
 		interfaces = new ArrayList<Interface>();
-		natTable = new NATTable();
+		natTable = new NATTable(() -> portAllocator.getAndIncrement());
 	}
 
 	public Router(String MACAddress) {
@@ -36,14 +44,20 @@ public class Router extends Node {
 	}
 
 	public static class Interface {
+		public enum Type {
+			PUBLIC,
+			PRIVATE
+		}
 		String MACAddress;
 		String ipAddress;
 		Subnet connectedSubnet;
+		Type type;
 
-		private Interface(String MACAddress, String ipAddress, Subnet subnet) {
+		private Interface(String MACAddress, String ipAddress, Subnet subnet, Type type) {
 			this.MACAddress = MACAddress;
 			this.ipAddress = ipAddress;
 			connectedSubnet = subnet;
+			this.type = type;
 		}
 
 		public static Builder builder() {
@@ -54,6 +68,7 @@ public class Router extends Node {
 			String MACAddress;
 			String ipAddress;
 			Subnet connectedSubnet;
+			Type type = Type.PRIVATE;
 
 			public Builder mac(String MACAddress) {
 				this.MACAddress = MACAddress;
@@ -70,13 +85,76 @@ public class Router extends Node {
 				return this;
 			}
 
+			public Builder type(Type type) {
+				this.type = type;
+				return this;
+			}
+
 			public Interface build() {
-				return new Interface(MACAddress, ipAddress, connectedSubnet);
+				return new Interface(MACAddress, ipAddress, connectedSubnet, type);
 			}
 		}
 	}
 
 	private class NATTable {
+		private Map<ConnectionKey, NATEntry> internalToExternal = new HashMap<>();
+		private Map<Integer, ConnectionKey> externalToInternal = new HashMap<>();
+		private final Supplier<Integer> portAllocator;
 
+		NATTable(Supplier<Integer> portAllocator) {
+			this.portAllocator = portAllocator;
+		}
+
+		private ConnectionKey translateInbound(int externalPort) {
+			return externalToInternal.get(externalPort);
+		}
+
+		private NATEntry translateOutbound(String internalIP, int internalPort) {
+			ConnectionKey key = new ConnectionKey(internalIP, internalPort);
+			if(!internalToExternal.containsKey(key)) {
+				int externalPort = portAllocator.get();
+				NATEntry entry = new NATEntry(internalIP, internalPort, externalIP, externalPort);
+				internalToExternal.put(key, entry);
+				externalToInternal.put(externalPort, key);
+			}
+			return internalToExternal.get(key);
+		}
+
+		private class ConnectionKey {
+			String ip;
+			int port;
+
+			ConnectionKey(String ip, int port) {
+				this.ip = ip;
+				this.port = port;
+			}
+
+			@Override
+			public boolean equals(Object o) {
+				if (this == o) return true;
+				if (o == null || getClass() != o.getClass()) return false;
+				ConnectionKey that = (ConnectionKey) o;
+				return port == that.port && Objects.equals(ip, that.ip);
+			}
+
+			@Override
+			public int hashCode() {
+				return Objects.hash(ip, port);
+			}
+		}
+
+		private class NATEntry {
+			String internalIP;
+			int internalPort;
+			String externalIP;
+			int externalPort;
+
+			NATEntry(String internalIP, int internalPort, String externalIP, int externalPort) {
+				this.internalIP = internalIP;
+				this.internalPort = internalPort;
+				this.externalIP = externalIP;
+				this.externalPort = externalPort;
+			}
+		}
 	}
 }
